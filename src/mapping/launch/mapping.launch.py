8aad39a904808d100fe3c1bf05e1220d1b28bbae
OsawaKousei <jwithelis@gmail.com>
2025-06-10 03:58:10 +0900

    slam成功

--- src/mapping/launch/mapping.launch.py
+++ src/mapping/launch/mapping.launch.py
@@ -28,70 +28,70 @@ from lifecycle_msgs.msg import Transition
 
 def generate_launch_description() -> LaunchDescription:
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    world_name = LaunchConfiguration('world_name', default='nav_slam_world')
+    world_name = LaunchConfiguration('world_name', default='test_world')
+    # get the package directory
     holonomic_sim_pkg_dir = get_package_share_directory('holonomic_sim')
-    pkg_share_dir = get_package_share_directory('mapping')
+    pkg_shared_dir = get_package_share_directory('mapping')
+    # get the model path
     model_path = os.path.join(holonomic_sim_pkg_dir, "models")
     autostart = LaunchConfiguration('autostart', default='true')
     use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager", default='true')
 
-    # ignition gazeboがモデルにアクセスできるように設定
+    # set the environment variable for the gazebo resource path
     ign_resource_path = SetEnvironmentVariable(
         name='IGN_GAZEBO_RESOURCE_PATH',
         value=[os.path.join("/opt/ros/humble", "share"), ":" + model_path],
     )
 
-    # ロボットをスポーンさせる設定
+    # spawn the robot
     ignition_spawn_entity = Node(
         package='ros_ign_gazebo',
         executable='create',
         output='screen',
         arguments=[
             '-entity',
-            'LidarRobo',
+            'HolonomicRobo',
             '-name',
-            'LidarRobo',
-            # ロボットのsdfファイルを指定
-            '-file',
-            PathJoinSubstitution(
-                [holonomic_sim_pkg_dir, "models", "LidarRobo", "model.sdf"]
-            ),
-            # ロボットの位置を指定
+            'HolonomicRobo',
+            # specify the robot description topic
+            '-topic',
+            'robot_description',
             '-allow_renaming',
             'true',
+            # set robot position
             '-x',
-            '0.4',
+            '0.5',
             '-y',
-            '0.4',
+            '0.5',
             '-z',
-            '0.075',
+            '1.0',
         ],
-        parameters=[{'use_sim_time': use_sim_time}],
     )
 
-    # フィールドをスポーンさせる設定
-    ignition_spawn_world = Node(
+    # spawn the field
+    ignition_spawn_field = Node(
         package='ros_ign_gazebo',
         executable='create',
         output='screen',
-        # フィールドのsdfファイルを指定
+        # specify the field path
         arguments=[
             '-file',
-            PathJoinSubstitution(
-                [holonomic_sim_pkg_dir, "models", "field", "model.sdf"]
-            ),
+            PathJoinSubstitution([model_path, "field", "model.sdf"]),
             '-allow_renaming',
             'false',
+            '-x',
+            '0.0',
+            '-y',
+            '0.0',
+            '-z',
+            '0.0',
         ],
-        parameters=[{'use_sim_time': use_sim_time}],
     )
 
-    # ワールドのsdfファイルを設定(worldタグのあるsdfファイル)
-    world = os.path.join(
-        holonomic_sim_pkg_dir, "models", "worlds", "holonomic_test.sdf"
-    )
+    # get the world file path
+    world = os.path.join(model_path, "worlds", "holonomic_test.sdf")
 
-    # ignition gazeboの起動設定
+    # launch the gazebo
     ign_gz = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -102,58 +102,69 @@ def generate_launch_description() -> LaunchDescription:
                 )
             ]
         ),
-        launch_arguments=[('ign_args', [' -r -v 3 ' + world])],
+        # specify the world file
+        launch_arguments=[('ign_args', [' -r -v 1 ' + world])],
     )
 
-    # ros_ign_bridgeの起動設定
+    # launch the message bridge
     bridge = Node(
         package='ros_ign_bridge',
         executable='parameter_bridge',
         parameters=[
             {
-                # brigdeの設定ファイルを指定
-                'config_file': os.path.join(pkg_share_dir, 'config', 'slam.yaml'),
+                # specify the configuration file
+                'config_file': os.path.join(
+                    holonomic_sim_pkg_dir, 'config', 'holonomic_test.yaml'
+                ),
+                # set the QoS profile
                 'qos_overrides./tf_static.publisher.durability': 'transient_local',
                 'qos_overrides./odom.publisher.durability': 'transient_local',
             },
             {'use_sim_time': use_sim_time},
         ],
+        # remap the topic
         remappings=[
             ("/odom/tf", "tf"),
         ],
         output='screen',
     )
 
-    # ロボットのsdfファイルのパスを取得
-    sdf = os.path.join(holonomic_sim_pkg_dir, 'models', 'LidarRobo', 'model.sdf')
+    # get the urdf file path
+    urdf = os.path.join(model_path, 'HolonomicUrdf', 'model.xacro')
+    # expand the xacro file
+    robot_desc = xacro.process_file(urdf).toxml()
 
-    # xacroでsdfファイルをurdfに変換
-    doc = xacro.parse(open(sdf))
-    xacro.process_doc(doc)
-
-    # robot_state_publsherの起動設定
+    # launch the robot state publisher
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='both',
-        parameters=[{'use_sim_time': True, 'robot_description': doc.toxml()}],
-    )  # type: ignore
+        arguments=[robot_desc],
+        # specify the robot description
+        parameters=[
+            {
+                'robot_description': robot_desc,
+                'use_sim_time': use_sim_time,
+            }
+        ],
+    )
 
-    # rviz2の設定フィルのパスを取得
-    rviz_config_dir = os.path.join(pkg_share_dir, 'config', 'mapping.rviz')
+    # get the rviz config file path
+    rviz_config_dir = os.path.join(pkg_shared_dir, 'config', 'mapping.rviz')
 
-    # rviz2の起動設定
+    # launch the rviz2
     rviz2 = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
+        # specify the config file
         arguments=['-d', rviz_config_dir],
         parameters=[{'use_sim_time': use_sim_time}],
         output='screen',
     )
 
-    slam_params_file = os.path.join(pkg_share_dir, 'config', 'slam_params.yaml')
+    slam_params_file = os.path.join(pkg_shared_dir, 'params', 'slam_params.yaml')
 
     start_async_slam_toolbox_node = LifecycleNode(
         parameters=[
@@ -206,17 +217,17 @@ def generate_launch_description() -> LaunchDescription:
         [
             ign_resource_path,
             ignition_spawn_entity,
-            ignition_spawn_world,
+            ignition_spawn_field,
             ign_gz,
             DeclareLaunchArgument(
                 'use_sim_time',
                 default_value=use_sim_time,
                 description='If true, use simulated clock',
             ),
+            bridge,
             DeclareLaunchArgument(
                 'world_name', default_value=world_name, description='World name'
             ),
-            bridge,
             robot_state_publisher,
             rviz2,
             start_async_slam_toolbox_node,
