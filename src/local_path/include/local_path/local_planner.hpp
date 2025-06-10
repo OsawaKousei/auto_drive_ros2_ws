03d9a909c805e962f88103b6fc035c9c113f24c2
OsawaKousei <jwithelis@gmail.com>
2025-06-10 15:09:03 +0900

    local_pathのインポート

---
+++ src/local_path/include/local_path/local_planner.hpp
@@ -0,0 +1,40 @@
+#ifndef LOCAL_PATH__LOCAL_PLANNER_HPP_
+#define LOCAL_PATH__LOCAL_PLANNER_HPP_
+
+#include "local_path/visibility_control.h"
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+namespace local_path
+{
+
+  class LocalPlanner : public rclcpp::Node
+  {
+  public:
+    TUTORIAL_PUBLIC
+    explicit LocalPlanner(const rclcpp::NodeOptions &options);
+    virtual ~LocalPlanner();
+
+  private:
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr local_path_pub_;
+
+    void path_callback(const nav_msgs::msg::Path::SharedPtr msg);
+
+    nav_msgs::msg::Path path_;
+    nav_msgs::msg::Odometry odom_;
+
+    std::vector<double> xs;
+    std::vector<double> ys;
+
+    int path_points_;
+    int consider_points_;
+
+    std::mutex mutex_;
+  };
+
+} // namespace local_path
+
+#endif // LOCAL_PATH__LOCAL_PLANNER_HPP_
---
+++ src/local_path/include/local_path/local_planning.hpp
@@ -0,0 +1,15 @@
+//
+// Created by emile on 24/05/06.
+//
+
+#ifndef LOCAL_PATH_LOCAL_PLANNING_HPP
+#define LOCAL_PATH_LOCAL_PLANNING_HPP
+
+#include <vector>
+// #include <planner_defines.hpp>
+
+std::pair<std::vector<double>, std::vector<double>> spline_by_num(std::vector<double> xs, std::vector<double> ys, const int &num_points);
+std::pair<std::vector<double>, std::vector<double>> spline_by_min_max(std::vector<double> xs, std::vector<double> ys,
+                                                                      double l_min, double l_max, double d_max);
+
+#endif // LOCAL_PATH_LOCAL_PLANNING_HPP
